82dacfd79065b58209201a233ede7dc4ddb993fa
Matheus Webler <matheus.webler@gmail.com>
2017-05-15 16:26:12 -0300

    added mongodb to travis-ci

--- config/index.js
+++ config/index.js
@@ -3,7 +3,7 @@ const port = 3000,
 let localMongoUrl = 'mongodb://localhost/mydb';
 
 // Use a local test database for integration tests
-if (process.env.NODE_ENV === 'test') localMongoUrl = 'mydb_test';
+if (process.env.NODE_ENV === 'test') localMongoUrl = 'mongodb://127.0.0.1/mydb_test';
 
 module.exports = {
     mongoUrl: localMongoUrl,
